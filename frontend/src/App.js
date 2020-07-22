a4f11700007653c2f30cab07b7f08ca9893a1384
danielmarques <danielvictorsm@gmail.com>
2020-07-22 10:09:28 -0300

    Page Main Building

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Painel de monitoramento</h1>
+      </header>
+      <aside>
+        <ul>
+          <li>Início</li>
+          <li>Mapa</li>
+          <li>Relatórios</li>
+          <li>Sobre</li>
+          <li>Contato</li>
+        </ul>
+      </aside>
+    </div>
+  );
+}
+
+export default App;
